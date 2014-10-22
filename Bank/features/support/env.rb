02cd2582e92ec23eeece374865979c74898f96d4
Trishul Gohil <trishulgohil@live.co.uk>
2014-10-22 21:55:21 +0100

    Cucumber book, Ch7,8

--- Bank/features/support/env.rb
+++ Bank/features/support/env.rb
@@ -1,3 +1,6 @@
-require 'rspec/expectations'
-require 'watir'
-require 'pry'
+require File.join(File.dirname(__FILE__), '..', '..', 'lib', 'nice_bank')
+
+require 'capybara/cucumber'
+
+Capybara.app = Sinatra::Application
+Sinatra::Application.set :environment, :test
